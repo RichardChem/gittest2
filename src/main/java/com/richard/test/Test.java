fec91973bc8cecaa26ccae260c84313b81e32eff
Richard <cq95525@gmail.com>
2021-04-17 07:16:54 +0800

    Create Class Test

---
+++ src/main/java/com/richard/test/Test.java
@@ -0,0 +1,14 @@
+package com.richard.test;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author richardchan
+ * @Date 2021/4/17 7:15 AM
+ * @Version 1.0
+ **/
+public class Test {
+    private Integer id;
+    private String name;
+    private String address;
+}
